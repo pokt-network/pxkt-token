793c5aaf4c11fa7691eecc1f6327ec8e20737706
Luis de Leon <luyzdeleon@gmail.com>
2019-03-05 12:08:02 -0400

    Added rinkeby network

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const INFURA_KEY = process.env.INFURA_KEY;
+
 module.exports = {
   networks: {
     local: {
@@ -8,6 +10,10 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5e9,
       network_id: '*'
+    },
+    rinkeby: {
+      host: 'https://rinkeby.infura.io/v3/' + INFURA_KEY,
+      network_id: '4'
     }
   }
 };
