06a01c4a48834c0528fb874167337a42264fac76
Luis de Leon <luyzdeleon@gmail.com>
2019-03-29 21:08:23 -0400

    Mainnet deployment

--- truffle-config.js
+++ truffle-config.js
@@ -4,8 +4,7 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 
 const INFURA_KEY = process.env.INFURA_KEY;
 const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
-console.log('Infura key: ' + INFURA_KEY);
-console.log('Rinkeby private key: ' + RINKEBY_PRIVATE_KEY);
+const MAINNET_PRIVATE_KEY = process.env.MAINNET_PRIVATE_KEY;
 
 module.exports = {
   networks: {
@@ -19,6 +18,10 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(RINKEBY_PRIVATE_KEY, "https://rinkeby.infura.io/v3/" + INFURA_KEY),
       network_id: '4'
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(MAINNET_PRIVATE_KEY, "https://mainnet.infura.io/v3/" + INFURA_KEY),
+      network_id: '1'
     }
   }
 };
